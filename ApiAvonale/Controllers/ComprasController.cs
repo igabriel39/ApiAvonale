59ecdd60abe75a4bdcb9de2e93ef8e0955810f52
igabriel39 <gabriel.mendonca2@hotmail.com>
2022-07-08 11:40:50 -0300

    Implementação da classe e controller para rota de comprar produto

---
+++ ApiAvonale/Controllers/ComprasController.cs
@@ -0,0 +1,20 @@
+﻿using ApiAvonale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ApiAvonale.Controllers
+{
+    public class ComprasController : ApiController
+    {
+        // Rota para realizar compra
+        public HttpResponseMessage Post( ParamCompra param )
+        {
+
+        }
+    }
+}
---
+++ ApiAvonale/Models/ParamCompra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAvonale.Models
+{
+    public class ParamCompra
+    {
+        public int produto_id { get; set; }
+        public int qtde_comprada { get; set; }
+        public Cartao cartao { get; set; }
+    }
+
+    public class Cartao
+    {
+        public string titular { get; set; }
+        public string numero { get; set; }
+        public DateTime data_expiracao { get; set; }
+        public string bandeira { get; set; }
+        public string cvv { get; set; }
+    }
+}
